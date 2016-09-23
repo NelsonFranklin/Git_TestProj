58f6cb77625956b7433ca0069da2e1344492751a
NelsonFranklin <nelson@atachisystems.com>
2016-09-23 17:34:16 +0530

    Testing

--- src/proj_source/Inventory.java
+++ src/proj_source/Inventory.java
@@ -5,6 +5,7 @@ public class Inventory {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Testing GIT");
+		System.out.println(" Testing GIT 23144");
 	}
 
 }
